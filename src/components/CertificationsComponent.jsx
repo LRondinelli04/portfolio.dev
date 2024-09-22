2d736edbc57f2e57efd53b67d0d1104dab5af095
LRondinelli04 <lrondinelli2004@gmail.com>
2024-09-22 18:42:01 -0300

    Agregado de seccion certificados

--- src/components/CertificationsComponent.jsx
+++ src/components/CertificationsComponent.jsx
@@ -1,10 +1,25 @@
 import { useState } from 'react';
+import LinkedInIcon from "@/components/LinkedInIcon";
+import CertificationsIcon from "@/components/CertificationsIcon";
+import SocialPill from "@/components/SocialPill";
 
 const CertificationsComponent = ({ certifications }) => {
   const [openCertification, setOpenCertification] = useState(null);
 
   const toggleSubCertifications = (index) => {
     setOpenCertification(openCertification === index ? null : index);
+  };
+
+  const transitionStyles = {
+    transition: 'max-height 500ms ease-in-out, opacity 500ms ease-in-out',
+    overflow: 'hidden',
+    opacity: 0,
+    maxHeight: 0,
+  };
+
+  const openStyles = {
+    opacity: 1,
+    maxHeight: '1000px', // Ajusta esto según sea necesario
   };
 
   return (
@@ -27,17 +42,19 @@ const CertificationsComponent = ({ certifications }) => {
             <p className="text-gray-700 dark:text-gray-400">{certification.issuer} - {certification.date}</p>
             <p className="mt-2 text-gray-700 dark:text-gray-400">{certification.description}</p>
             <div className="flex items-start justify-start mt-4 gap-x-4">
-              <a href={certification.certificateLink} className="link-button" target="_blank" rel="noopener noreferrer">
-                Ver Certificado
-              </a>
-              <a href={certification.linkedinLink} className="link-button" target="_blank" rel="noopener noreferrer">
-                Publicación en LinkedIn
-              </a>
+              <SocialPill href={certification.certificateLink} target="_blank" rel="noopener noreferrer">
+                <CertificationsIcon className="size-4" />
+                Certificado
+              </SocialPill>
+              <SocialPill href={certification.linkedinLink} target="_blank" rel="noopener noreferrer">
+                <LinkedInIcon className="size-4" />
+                <p> Publicación </p>
+              </SocialPill>
               {/* Botón Ver más para desplegar sub-certificaciones */}
               {certification.subCertifications && (
                 <button
-                  className="link-button"
-                  onClick={() => toggleSubCertifications(index)}
+                  className="link-button mt-2"
+                  onClick={() => toggleSubCertifications(index)} // REACT maneja el evento
                 >
                   {openCertification === index ? "Ver menos" : "Ver más"}
                 </button>
@@ -45,21 +62,37 @@ const CertificationsComponent = ({ certifications }) => {
             </div>
 
             {/* Mostrar sub-certificaciones si están disponibles y activas */}
-            {certification.subCertifications && openCertification === index && (
-              <div className="mt-4">
-                <h4 className="text-lg font-bold text-gray-800 dark:text-gray-100">Sub-certificaciones:</h4>
-                <ul className="list-disc ml-4 text-gray-700 dark:text-gray-400">
-                  {certification.subCertifications.map((subCert, subIndex) => (
-                    <li key={subIndex}>
-                      <p>{subCert.title} - {subCert.date}</p>
-                      <a href={subCert.certificateLink} className="link-button" target="_blank" rel="noopener noreferrer">
-                        Ver Certificado
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            )}
+            <div
+              style={{
+                ...transitionStyles,
+                ...(openCertification === index ? openStyles : {}),
+              }}
+            >
+              {certification.subCertifications && openCertification === index && (
+                <div className="mt-4">
+                  <h4 className="text-lg font-bold text-gray-800 dark:text-gray-100">Certificaciones:</h4>
+                  <ul className="list-disc ml-4 text-gray-700 dark:text-gray-400">
+                    {certification.subCertifications.map((subCert, subIndex) => (
+                      <li key={subIndex} className="mt-5">
+                        <p><strong>{subCert.title}</strong> - {subCert.date}</p>
+                        <div className="flex items-center mt-2">
+                          <SocialPill href={subCert.certificateLink} target="_blank" rel="noopener noreferrer">
+                            <CertificationsIcon className="size-4" />
+                            Certificado
+                          </SocialPill>
+                          <span className="ml-2">
+                            <SocialPill href={subCert.linkedinLink} target="_blank" rel="noopener noreferrer">
+                              <LinkedInIcon className="size-4" />
+                              Publicación
+                            </SocialPill>
+                          </span>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+            </div>
           </div>
         </article>
       ))}
