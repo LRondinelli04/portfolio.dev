c295e4cddb9a16a249e443385afd9ed913423a04
Lucas Rondinelli <lrondinelli@fepba.gov.ar>
2024-09-23 12:36:36 -0300

    formateo de archivos para una mejor organizacion

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 
-import robotsTxt from "astro-robots-txt"
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt(), react()],
-  site: 'https://porfolio.dev/'
-})
+  site: "https://porfolio.dev/",
+});
