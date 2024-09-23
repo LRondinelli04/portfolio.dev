566c65dacdaea38bc9e5fd82c88084a930d78fb3
Lucas Rondinelli <lrondinelli@fepba.gov.ar>
2024-09-23 10:10:36 -0300

    agregado de estilos a boton verMas/verMenos

--- src/components/CertificationsComponent.jsx
+++ src/components/CertificationsComponent.jsx
@@ -53,7 +53,7 @@ const CertificationsComponent = ({ certifications }) => {
               {/* Botón Ver más para desplegar sub-certificaciones */}
               {certification.subCertifications && (
                 <button
-                  className="link-button mt-2"
+                className={"inline-flex items-center justify-center gap-2 px-4 py-1 text-gray-800 transition bg-gray-100 border border-gray-300 rounded-full dark:bg-gray-800 dark:border-gray-600 dark:text-white focus-visible:ring-yellow-500/80 text-md hover:bg-gray-900 hover:border-gray-700 hover:text-white dark:hover:bg-gray-100 dark:hover:border-gray-300 dark:hover:text-black group max-w-fit focus:outline-none focus-visible:outline-none focus-visible:ring focus-visible:ring-white focus-visible:ring-offset-2 active:bg-black"}
                   onClick={() => toggleSubCertifications(index)} // REACT maneja el evento
                 >
                   {openCertification === index ? "Ver menos" : "Ver más"}
